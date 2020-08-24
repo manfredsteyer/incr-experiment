013b767f6b4911d0b2407f0877fe0c97dc3008f8
Manfred Steyer <manfred.steyer@gmx.net>
2020-08-24 21:49:08 +0200

    add hierarchical structure

--- generate-commands.js
+++ generate-commands.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
+
 const count = 100; // 10, 20, 100, 200
 const existing = 0; // there are already two features
 
+let content = '';
 for(let i = 0; i<count-existing; i++) {
-    for (let j=0; j<30; j++) {
-        console.log(`call ng g component feature${i}-component${j} --project luggage-feature-f${i}`);
+    // console.log(`<luggage-f${i}></luggage-f${i}>`);
+
+    for (let j=1; j<30; j++) {
+        content += `<luggage-feature${i}-component${j}></luggage-feature${i}-component${j}>\n`;
     }
+
+    const path = `libs/luggage/feature-f${i}/src/lib/f${i}.component.html`;
+    fs.writeFileSync(path, content);
+
+    content = '';
 }
